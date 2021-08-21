0c2bc20b0fa577b6260ec014a507bd71788d223c
AssisrMatheus <assisr.matheus@gmail.com>
2021-08-21 11:27:34 -0300

    nwjs post message

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
+const { GlobalKeyboardListener } = require("node-global-key-listener");
+
 const startUrl = process.env.NWJS_START_URL || '../build/index.html';
 
-nw.Window.open(startUrl, {}, function(win) {});
+const keyboardListener = new GlobalKeyboardListener();
+
+nw.Window.open(startUrl, {}, function(win) {
+    win.on('loaded', function (){
+        keyboardListener.addListener(function (e, down) {
+            win.window.postMessage(JSON.stringify({ e, down }), "*"); //the second parameter specifies the message origin. afaik, this is merely a string and has no effect beyond being there
+        })
+    });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const keyboardListener = new GlobalKeyboardListener();
+
+// console.log(os.platform());
+
 function App() {
+
+  const [val, setVal] = useState("");
+  useEffect(() => {
+    setVal(window !== undefined ? 'has window' : "no window");
+    window.addEventListener('message', function(event) {
+      setVal(event.data);
+      var data = JSON.parse(event.data);
+      console.log(data);
+    }, false);
+
+    // //Log every key that's pressed.
+    // keyboardListener.addListener((e, down) => {
+    //   const rawValue = e.vKey;
+    //   const code = codes[`${rawValue}`];
+    //
+    //   let string = "";
+    //
+    //   const ctrlKey =
+    //       (down["LEFT CTRL"] || down["RIGHT CTRL"]) &&
+    //       e.name !== "LEFT CTRL" &&
+    //       e.name !== "RIGHT CTRL";
+    //
+    //   const shiftKey =
+    //       (down["LEFT SHIFT"] || down["RIGHT SHIFT"]) &&
+    //       e.name !== "LEFT SHIFT" &&
+    //       e.name !== "RIGHT SHIFT";
+    //
+    //   const altKey =
+    //       (down["LEFT ALT"] || down["RIGHT ALT"]) &&
+    //       e.name !== "LEFT ALT" &&
+    //       e.name !== "RIGHT ALT";
+    //
+    //   if (
+    //       e.state == "DOWN" &&
+    //       (ctrlKey || shiftKey || altKey || whitelist[`${rawValue}`])
+    //   ) {
+    //     if (ctrlKey) string = string.concat(codes[17], " + ");
+    //
+    //     if (shiftKey) string = string.concat(codes[16], " + ");
+    //
+    //     if (altKey) string = string.concat(codes[18], " + ");
+    //
+    //     if (code) string = string.concat(code);
+    //   }
+    //
+    //   if (string) setVal(string);
+    // });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {val}
         </p>
         <a
           className="App-link"
